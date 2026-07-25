713c3033e05e1ca3a41b19889321b2862fb45552
Knightwing1941 <pavik14@live.com>
2026-07-25 15:32:11 -0500

    Add basic nav bar to the application

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,21 @@
 import './App.css'
 import Home from './pages/Home'
+import Favorites from './pages/Favorites'
+import NavBar from './components/Navbar'
+import { Routes, Route } from "react-router-dom"
 
 function App() {
 
   return (
     <div>
-      <Home />
+      <NavBar />
+      <main className='main-content'>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favorites" element={<Favorites />} />
+          <Route path="*" element={<div>Page Not Found</div>} />
+        </Routes>
+      </main>
     </div>
   )
 }
